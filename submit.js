8c6c161baa7f53e97bdc4683ec729120e736cf7f
LEONG CHEE YAN <127811480+cycy96@users.noreply.github.com>
2023-05-16 23:44:24 +0800

    Added index.html and submit.js

---
+++ submit.js
@@ -0,0 +1,13 @@
+
+        function checkGender(){				
+            if(document.getElementById('radioMale').checked)
+            {
+                alert("Gender selected: Male.");
+                window.open("male.html");
+            }
+            else
+            {
+                alert("Gender selected: Female.");
+                window.open("female.html");
+            }
+        }
